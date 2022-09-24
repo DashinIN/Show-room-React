ded589ba21aa09f63bf59855419465e56ba50611
DashinIN <green19grant@yandex.ru>
2022-09-24 19:01:45 +0300

    2nd skeleton

--- src/pages/Moideti/Moideti.jsx
+++ src/pages/Moideti/Moideti.jsx
@@ -1,16 +1,15 @@
-
+import s from "./moideti.module.scss"
 
 const Moideti = () => {
 
     document.title ="Мои дети не будут скучать";
 
-    
- 
-
     return (
-        <>
-      <p>moideti</p>
-        </>
+      <div className={s.main}>
+        <div className={s.item}></div>
+        <div className={s.item}></div>
+        <div className={s.item}></div>
+      </div>
     );
 }
 
--- src/pages/Perezvoni/Perezvoni.jsx
+++ src/pages/Perezvoni/Perezvoni.jsx
@@ -4,36 +4,26 @@ const Perezvoni = () => {
 
     document.title ="Перезвони мне";
     return (
-
         <div className={s.main}>
-            <div className={`${s.column} ${s.column1}`}>
-                <div className={`${s.item} ${s.item1}`}>
-
+            <div className={s.column} >
+                <div className={s.item}>   
                 </div>
-                <div className={`${s.item} ${s.item2}`}>
-                    
+                <div className={s.item}>     
                 </div>
             </div>
-
-            <div className={`${s.column} ${s.column2}`}>
-                <div className={`${s.item} ${s.item1}`}>
-
+            <div className={s.column} >
+                <div className={s.item}>     
                 </div>
-                <div className={`${s.item} ${s.item2}`}>
-                    
+                <div className={s.item}>  
                 </div>
             </div>
-
-            <div className={`${s.column} ${s.column3}`}>
-                <div className={`${s.item} ${s.item1}`}>
-
+            <div className={s.column} >
+                <div className={s.item}>
                 </div>
-                <div className={`${s.item} ${s.item2}`}>
-                    
+                <div className={s.item}>
                 </div>
             </div>
         </div>
-        
     );
 }
 
